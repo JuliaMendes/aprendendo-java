0e444377cb6d6122da6157ae9d3b88f4d31144e2
Júlia Mendes <juliadepaivams@gmail.com>
2020-09-10 20:03:25 -0300

    Aprendendo Java

---
+++ Temperatura/src/module-info.java
@@ -0,0 +1,2 @@
+module Temperatura {
+}
---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+/**
+ * 
+ * @author Júlia Mendes <juliamendesdev@gmail.com>
+ * @since JDK1.0
+ * @see 
+ *
+ */
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		//Uma sentença de código termina com ;
+		// Mais uma linha...
+		// Fim
+		System.out.println("Primeiro programa parte #01!!");
+		
+		/*
+		 * Linha 1
+		 * Linha 2....
+		 */
+		System.out.println("Primeiro programa parte #02!!");
+	}
+}
