0a885652b87956db3edfc38a91185977816e51cc
Emmanuel Kant Focat Almeida Duarte <emmanuel.duarte@b2wdigital.com>
2020-09-25 17:59:12 -0300

    Aprendendo Java

--- exercicios/src/fundamentos/ParaBrincar.java
+++ exercicios/src/fundamentos/ParaBrincar.java
@@ -1,8 +1,15 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class ParaBrincar {
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner (System.in);
+		
+		
+			
+		
 	
-		System.out.println("Oi tudo bem".hashCode());
+	
 }
 }
